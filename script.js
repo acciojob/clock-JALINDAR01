be3d3d3887e8f9f526c1b692563ff7f6d93a33b6
JALINDAR01 <134129652+JALINDAR01@users.noreply.github.com>
2023-09-15 13:48:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function livetime() {
+	let date=new Date();
+	let day=data.getDate();
+	let month=date.getMonth()+1;
+	let year=data.getFullyear();
+	let time=date.toLocaleTimeString('en-US',{hour:'numeric',minute:'numeric',second:'numeric',hour12:true});
+	let cudate = `${month}/${day}/${year}`;
+	document.getElementById('timer').textContent=cudate+','+time;
+	
+}
+function startTimer() {
+	livetime();
+	setInterval(livetime,1000);
+}
+window.addEventListener('load',startTimer);
+
